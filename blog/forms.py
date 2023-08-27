bf19562b30be602ba65f41f9367e11f610fb0af5
keinermendoza <keinermendoza@gmail.com>
2023-08-26 22:17:14 -0400

    edditing forms

--- blog/forms.py
+++ blog/forms.py
@@ -4,12 +4,37 @@ from crispy_forms.layout import Submit
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
+
     class Meta:
+        required_message = "Este campo es obligatorio"
+
         model = Comment
         fields = ["name", "email", "body"]
 
         widgets = {
             "body": forms.Textarea(attrs={"rows":4})
+        }
+        error_messages = {
+            "name": {
+                "required": required_message,
+                "max_length": "Este campo acepta hasta 120 caracteres"
+            },
+            "email": {
+                "required": required_message,
+                "invalid": "Email invalido"
+            },
+            "body": {
+                "required": required_message,
+            }
+        }
+        help_texts = {
+            "email": "El email se mantendrá en privado",
+        }
+
+        labels = {
+            "name": "Nombre",
+            "email": "Email",
+            "body": "Comentario",
         }
 
     def __init__(self, *args, **kwargs):
@@ -23,6 +48,25 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField()
     comment = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows":4}))
+
+    class Meta:
+        required_message = "Este campo es obligatorio"
+        invalid_message = "Email invalido"
+
+        error_messages = {
+            "name": {
+                "required": required_message,
+                "max_length": "Este campo acepta hasta 120 caracteres"
+            },
+            "email": {
+                "required": required_message,
+                "invalid": invalid_message,
+            },
+            "to": {
+                "required": required_message,
+                "invalid": invalid_message,
+            },
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
