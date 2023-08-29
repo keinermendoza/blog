33665b89740feffe37d4fc78e8599d0e94ab66ee
keinermendoza <keinermendoza@gmail.com>
2023-08-28 20:57:12 -0400

    cleaning

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views, api_views
 from .feed import LatestPostFeed
+
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("posts/tag/<slug:tag_slug>", views.post_list_by_tag, name="post_list_by_tag"),
-
     path("post/<slug:post_slug>/<int:day>/<int:month>/<int:year>", views.post_detail, name="post_detail"),
     path('post/<int:post_id>/share', views.post_share, name="post_share"),
     path('post/<int:post_id>/comment', views.post_comment, name="post_comment"),
@@ -12,9 +12,4 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout_view, name='logout'),
-
-
-    # api_views
-    path("posts/published_posts", api_views.published_posts, name="published_posts"),
-
 ]
