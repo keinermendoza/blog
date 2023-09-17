e4c14c181ea89056aad722d95ef89d3dd14c3e7f
keinermendoza <keinermendoza@gmail.com>
2023-09-17 09:07:03 -0400

    manual test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,53 +1,51 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-# kill any servers that may be running in the background 
-sudo pkill -f runserver
+# # kill any servers that may be running in the background 
+# sudo pkill -f runserver
 
-# kill frontend servers if you are deploying any frontend
-# sudo pkill -f tailwind
-# sudo pkill -f node
+# # kill frontend servers if you are deploying any frontend
+# # sudo pkill -f tailwind
+# # sudo pkill -f node
 
-cd /home/ubuntu/blog/
+# cd /home/ubuntu/blog/
 
-# activate virtual environment
-python3 -m venv venv
-source venv/bin/activate
+# # activate virtual environment
+# python3 -m venv venv
+# source venv/bin/activate
 
-# install requirements.txt
-pip install -r /home/ubuntu/blog/requirements.txt
+# # install requirements.txt
+# pip install -r /home/ubuntu/blog/requirements.txt
 
-# puting staticfiles on a single static folder
-python3 manage.py collectstatic
+# # puting staticfiles on a single static folder
+# python3 manage.py collectstatic
 
-# ubicandome en la base
-cd
+# # ubicandome en la base
+# cd
 
-# instalando nginx
-sudo apt install nginx -y
+# # instalando nginx
+# sudo apt install nginx -y
 
-# iniciando gunicorn modo daemon
-gunicorn -c conf/gunicorn_config.py blog.mysite.wsgi -D
+# # iniciando gunicorn modo daemon
+# gunicorn -c conf/gunicorn_config.py blog.mysite.wsgi -D
 
-# iniciando nginx
-sudo service ngnix start
+# # iniciando nginx
+# sudo service ngnix start
 
-# remove default nginx conf sites-avaliable
-# sudo rm --f /etc/nginx/sites-available/default
 
-# copy my custom conf sites-available
-sudo cp home/ubuntu/blog/sites-available/blog /etc/nginx/sites-available
+# # copy my custom conf sites-available
+# sudo cp home/ubuntu/blog/sites-available/blog /etc/nginx/sites-available
 
-# entry to nginx folder
-cd /etc/nginx/sites-enabled/
+# # entry to nginx folder
+# cd /etc/nginx/sites-enabled/
 
-# linkig to my project
-sudo ln -s /etc/nginx/sites-available/blog
+# # linkig to my project
+# sudo ln -s /etc/nginx/sites-available/blog
 
-# restarting ngnix
-sudo systemctl restart nginx
+# # restarting ngnix
+# sudo systemctl restart nginx
 
-# migrate database sqlite for test
-python3 manage.py migrate
+# # migrate database sqlite for test
+# python3 manage.py migrate
 
-# run server
-screen -d -m python3 manage.py runserver 0:8000
+# # run server
+# screen -d -m python3 manage.py runserver 0:8000
