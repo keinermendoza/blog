5966d5e52630ba5b6bf1d8ffa4502898bd886489
keinermendoza <keinermendoza@gmail.com>
2023-09-24 09:18:46 -0400

    again

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+# go into the blog floder
+cd home/ubuntu/blog
+
 # get direction before install
 sudo pwd > aqui.txt
 sudo mv aqui.txt /home/
-
-# go into the blog floder
-cd home/ubuntu/blog
 
 # Create images using the docker-compose
 sudo docker compose build
